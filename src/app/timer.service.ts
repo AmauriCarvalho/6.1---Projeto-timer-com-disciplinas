05063d1f8cabc6be36305595b555e160a781846d
AmauriCarvalho <amauri.carvalho01@fatec.sp.gov.br>
2021-09-30 17:38:33 -0300

    3 files modified

--- src/app/timer.service.ts
+++ src/app/timer.service.ts
@@ -4,24 +4,22 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class TimerService {
   private timer: any;
-  private counter = 0;
-  c = 0;
-  f = 0; 
+  private counter = 0; 
   constructor() {
    }
-   start(ms: number){
-      if(!this.timer && this.c > 0){
-        this.counter = this.c;
-        this.timer = setInterval(()=>{
-          this.counter++;
-          if(this.f == this.counter){
-            clearInterval(this.timer);
-            this.timer=null;
-          }
-        }, ms);
+   comeco(){
+    if(!this.timer){
+      this.counter = 0;
+      this.timer = setInterval(()=>{
+        this.counter++;
+        if(this.counter == 60){
+          this.parar();
+          this.comeco();
+        }
+      }, 1000)
       }
-  }
-  stop(){
+    }
+  parar(){
     if(this.timer){
       clearInterval(this.timer);
       this.timer=null;
